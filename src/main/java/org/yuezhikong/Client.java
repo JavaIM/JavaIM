76804ad39e4e40f0e9d6ca0430171985e30176e3
QiLechan <qilechan@outlook.com>
2023-02-04 15:04:14 +0800

    通信实现中

--- src/main/java/org/yuezhikong/Client.java
+++ src/main/java/org/yuezhikong/Client.java
@@ -15,5 +15,22 @@ public class Client {
         DataInputStream in = new DataInputStream(inFromServer);
         System.out.println("服务器响应： " + in.readUTF());
 
+        BufferedWriter writer = null;
+        BufferedReader reader = null;
+        BufferedReader consoleReader = null;
+
+        while (true) {
+            reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+            // 等待用户输入信息
+            consoleReader = new BufferedReader(new InputStreamReader(System.in));
+            String input = consoleReader.readLine();
+            // 发送消息给服务器
+            writer.write(input + "\n");
+            writer.flush();
+            // 读取服务器返回的消息
+            String msg = reader.readLine();
+            System.out.println(msg);
+        }
     }
 }
--- src/main/java/org/yuezhikong/Server.java
+++ src/main/java/org/yuezhikong/Server.java
@@ -4,7 +4,7 @@ import java.net.*;
 import java.io.*;
 
 public class Server {
-    public Server(int port) {
+    public Server(int port) throws IOException {
         ServerSocket serverSocket = null;
         try {
             serverSocket = new ServerSocket(port);
@@ -12,23 +12,28 @@ public class Server {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         while (true) {
             try {
                 System.out.println("等待远程连接，端口号为：" + serverSocket.getLocalPort() + "...");
                 Socket server = serverSocket.accept();
+                Socket server2 = serverSocket.accept();
                 System.out.println("远程主机地址：" + server.getRemoteSocketAddress());
                 DataInputStream in = new DataInputStream(server.getInputStream());
                 System.out.println(in.readUTF());
                 DataOutputStream out = new DataOutputStream(server.getOutputStream());
                 out.writeUTF("服务器连接成功：" + server.getLocalSocketAddress());
+
                 BufferedReader reader = null;
                 BufferedWriter writer = null;
+
                 reader = new BufferedReader(new InputStreamReader(server.getInputStream()));
-                writer = new BufferedWriter(new OutputStreamWriter(server.getOutputStream()));
+                writer = new BufferedWriter(new OutputStreamWriter(server2.getOutputStream()));
                 String msg = null;
                 while ((msg = reader.readLine()) != null) {
                     // 读取客户端发送的消息
                     System.out.println("客户端[" + server.getInetAddress() + ":" + server.getPort() + "]: " + msg);
+                    writer.write("服务器已收到: " + msg + "\n");
                 }
             } catch (SocketTimeoutException s) {
                 System.out.println("Socket timed out!");
@@ -38,5 +43,35 @@ public class Server {
                 break;
             }
         }
+        ServerSocket finalServerSocket = serverSocket;
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                BufferedReader reader2 = null;
+                BufferedWriter writer2 = null;
+                while (true) {
+                    try {
+                        Socket server = finalServerSocket.accept();
+                        Socket server2 = finalServerSocket.accept();
+                        reader2 = new BufferedReader(new InputStreamReader(server2.getInputStream()));
+                        writer2 = new BufferedWriter(new OutputStreamWriter(server.getOutputStream()));
+                        String msg2 = null;
+                        while ((msg2 = reader2.readLine()) != null) {
+                            // 读取客户端发送的消息
+                            System.out.println("客户端[" + server2.getInetAddress() + ":" + server2.getPort() + "]: " + msg2);
+                            writer2.write("服务器已收到: " + msg2 + "\n");
+                        }
+                    } catch (SocketTimeoutException s) {
+                        System.out.println("Socket timed out!");
+                        break;
+                } catch (IOException e) {
+                        e.printStackTrace();
+                        break;
+                    }
+                }
+            }
+        };
+        Thread thread = new Thread(runnable);
+        thread.start();
     }
 }
