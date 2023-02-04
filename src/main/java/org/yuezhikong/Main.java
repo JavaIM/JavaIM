2ef5522fa6a18ceb99c80d37544d0a05ae24ce5a
AlexLiuDev233 <wzylin11@outlook.com>
2023-02-04 20:36:51 +0800

    铲除了老服务端

--- src/main/java/org/yuezhikong/Main.java
+++ src/main/java/org/yuezhikong/Main.java
@@ -9,7 +9,7 @@ public class Main {
     //public static final Logger logger = LogManager.getLogger(Main.class);
     public static final Logger logger = Logger.getGlobal();
     public static void main(String[] args) {
-        logger.info("使用客户端模式请输入1，服务端模式请输入2，测试性新服务端模式请输入3:");
+        logger.info("使用客户端模式请输入1，服务端模式请输入2:");
         Scanner sc = new Scanner(System.in);
         int mode = sc.nextInt();
         if (mode == 1) {
@@ -22,13 +22,6 @@ public class Main {
             int port = Integer.parseInt(sc3.nextLine());
             new Client(serverName,port);
         } else if (mode == 2) {
-            Scanner sc4 = new Scanner(System.in);
-            logger.info("请输入监听端口:");
-            int port = Integer.parseInt(sc4.nextLine());
-            new Server(port);
-        }
-        else if (mode == 3)
-        {
             Scanner sc4 = new Scanner(System.in);
             logger.info("请输入监听端口:");
             int port = Integer.parseInt(sc4.nextLine());
