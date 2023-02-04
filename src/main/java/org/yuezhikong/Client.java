d159db23f54402a072965c4125bfc1c869d86d7c
AlexLiuDev233 <wzylin11@outlook.com>
2023-02-04 20:30:49 +0800

    加入客户端消息通过服务端转发功能 在源码中加入config 修改System.out.println、System.out.print为日志调用

--- src/main/java/org/yuezhikong/Client.java
+++ src/main/java/org/yuezhikong/Client.java
@@ -3,19 +3,52 @@ package org.yuezhikong;
 import java.io.*;
 import java.net.Socket;
 import java.util.logging.Logger;
+//import java.util.logging.Logger;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
 
 public class Client {
+    //public static final Logger logger = LogManager.getLogger(Client.class);
+    public static final Logger logger = Logger.getGlobal();
+    private Socket client;
     public Client(String serverName, int port) {
         try {
-            System.out.println("连接到主机：" + serverName + " ，端口号：" + port);
-            Socket client = new Socket(serverName, port);
-            System.out.println("远程主机地址：" + client.getRemoteSocketAddress());
+            logger.info("连接到主机：" + serverName + " ，端口号：" + port);
+            client = new Socket(serverName, port);
+            logger.info("远程主机地址：" + client.getRemoteSocketAddress());
             OutputStream outToServer = client.getOutputStream();
             DataOutputStream out = new DataOutputStream(outToServer);
             out.writeUTF("Hello from " + client.getLocalSocketAddress());
             InputStream inFromServer = client.getInputStream();
             DataInputStream in = new DataInputStream(inFromServer);
-            System.out.println("服务器响应： " + in.readUTF());
+            logger.info("服务器响应： " + in.readUTF());
+
+            Runnable runnable = () ->
+            {
+                while (true)
+                {
+                    BufferedReader reader;//获取输入流
+                    try {
+                        reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                        String msg = reader.readLine();
+                        logger.info(msg);
+                    }
+                    catch (IOException e)
+                    {
+                        if (!"Connection reset by peer".equals(e.getMessage())) {
+                            logger.warning("发生I/O错误");
+                            e.printStackTrace();
+                        }
+                        else
+                        {
+                            logger.info("连接早已被关闭...");
+                            break;
+                        }
+                    }
+                }
+            };
+            Thread thread = new Thread(runnable);
+            thread.start();
 
             BufferedWriter writer;
             BufferedReader consoleReader;
@@ -28,7 +61,7 @@ public class Client {
                 // 检查用户输入是否是quit
                 if ("quit".equals(input))
                 {
-                    Logger.getGlobal().info("正在断开连接");
+                    logger.info("正在断开连接");
                     writer.write(input + "\n");
                     client.close();
                     break;
@@ -37,12 +70,18 @@ public class Client {
                 writer.write(input + "\n");
                 writer.flush();
             }
-            Logger.getGlobal().info("再见~");
+            logger.info("再见~");
         }
         catch (IOException e)
         {
-            Logger.getGlobal().warning("发生I/O错误");
-            e.printStackTrace();
+            if (!"Connection reset by peer".equals(e.getMessage())) {
+                logger.warning("发生I/O错误");
+                e.printStackTrace();
+            }
+            else
+            {
+                logger.info("连接早已被关闭...");
+            }
         }
     }
 }
