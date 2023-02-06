1006627cfab3e51b34d557d30a1beb6dd16660a1
AlexLiuDev233 <wzylin11@outlook.com>
2023-02-06 17:27:51 +0800

    修复了RSA加密系统 修复了明文加密系统 服务端加入了quit命令 客户端加入了服务端公钥文件是否存在的检测 请注意，本次提交启用了RSA系统，若不需要，请在config.java中关闭！

--- src/main/java/org/yuezhikong/config.java
+++ src/main/java/org/yuezhikong/config.java
@@ -4,6 +4,8 @@ public class config {
     private static final int MAX_CLIENT = -1;
     private static final boolean Debug_Mode = false;
     private static final boolean Auto_Save_Dependency = false;
+    private static final boolean Test_RSA_Mode = true;
+    public static boolean GetRSA_Mode (){ return Test_RSA_Mode; }
     public static int getMaxClient()
     {
         return MAX_CLIENT;
