ca0470478e1e5922162e4ce5b74c4018cc97d8e0
七楽 <88995341+QiLechan@users.noreply.github.com>
2023-02-18 15:53:34 +0800

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,8 +1,6 @@
 apt update
-apt -y install git
 apt -y install openjdk-17-jdk
 apt -y install maven
-git clone https://github.com/QiLechan/JavaIM.git
 cd JavaIM && sh build.sh
 wget https://download.bell-sw.com/java/17.0.6+10/bellsoft-jre17.0.6+10-linux-amd64.deb && dpkg -i bellsoft-jre17.0.6+10-linux-amd64.deb
 apt -y remove maven
