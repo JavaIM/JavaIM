e916c56af4a67c1afa23faa791e74994ca1d6e3b
AlexLiuDev233 <wzylin11@outlook.com>
2023-05-13 11:24:53 +0800

    让代码可读性增加

--- src/main/java/org/yuezhikong/Server/timer.java
+++ src/main/java/org/yuezhikong/Server/timer.java
@@ -2,6 +2,7 @@ package org.yuezhikong.Server;
 
 import org.yuezhikong.CodeDynamicConfig;
 import org.yuezhikong.Server.UserData.user;
+import org.yuezhikong.Server.api.ServerAPI;
 import org.yuezhikong.utils.DataBase.Database;
 import org.yuezhikong.utils.Logger;
 import org.yuezhikong.utils.SaveStackTrace;
@@ -28,54 +29,38 @@ public class timer
         @Override
         public void run() {
             //从这里开始写时间事件
-            int i = 0;
-            int tmpClientIDAll = Server.GetInstance().getClientIDAll();
-            tmpClientIDAll = tmpClientIDAll - 1;
-            while (true) {
-                if (i > tmpClientIDAll) {
-                    break;
-                }
-                user ProcessUser = Server.GetInstance().getUsers().get(i);
-                if (ProcessUser.GetUserSocket() == null) {
-                    i = i + 1;
-                    continue;
-                }
-                if (ProcessUser.GetUserLogined()) {
-                    if (ProcessUser.isMuted()) {
-                        Date date = new Date();
-                        long Time = date.getTime();//获取当前时间毫秒数
-                        if (ProcessUser.getMuteTime() <= Time) {
-                            ProcessUser.setMuteTime(0);
-                            ProcessUser.setMuted(false);
-                            Runnable SQLUpdateThread = () -> {
-                                try {
-                                    Connection DatabaseConnection = Database.Init(CodeDynamicConfig.GetMySQLDataBaseHost(), CodeDynamicConfig.GetMySQLDataBasePort(), CodeDynamicConfig.GetMySQLDataBaseName(), CodeDynamicConfig.GetMySQLDataBaseUser(), CodeDynamicConfig.GetMySQLDataBasePasswd());
-                                    String sql = "UPDATE UserData SET UserMuted = 0 and UserMuteTime = 0 where UserName = ?";
-                                    PreparedStatement ps = DatabaseConnection.prepareStatement(sql);
-                                    ps.setString(1, ProcessUser.GetUserName());
-                                    ps.executeUpdate();
-                                    DatabaseConnection.close();
-                                } catch (ClassNotFoundException | SQLException e) {
-                                    SaveStackTrace.saveStackTrace(e);
-                                }
-                            };
-                            Thread UpdateThread = new Thread(SQLUpdateThread);
-                            UpdateThread.start();
-                            UpdateThread.setName("SQL Update Thread");
+            for (user ProcessUser : ServerAPI.GetValidClientList(Server.GetInstance(),true))
+            {
+                if (ProcessUser.isMuted()) {
+                    Date date = new Date();
+                    long Time = date.getTime();//获取当前时间毫秒数
+                    if (ProcessUser.getMuteTime() <= Time) {
+                        ProcessUser.setMuteTime(0);
+                        ProcessUser.setMuted(false);
+                        Runnable SQLUpdateThread = () -> {
                             try {
-                                UpdateThread.join();
-                            } catch (InterruptedException e) {
-                                Logger logger = Server.GetInstance().logger;
-                                logger.error("发生异常InterruptedException");
+                                Connection DatabaseConnection = Database.Init(CodeDynamicConfig.GetMySQLDataBaseHost(), CodeDynamicConfig.GetMySQLDataBasePort(), CodeDynamicConfig.GetMySQLDataBaseName(), CodeDynamicConfig.GetMySQLDataBaseUser(), CodeDynamicConfig.GetMySQLDataBasePasswd());
+                                String sql = "UPDATE UserData SET UserMuted = 0 and UserMuteTime = 0 where UserName = ?";
+                                PreparedStatement ps = DatabaseConnection.prepareStatement(sql);
+                                ps.setString(1, ProcessUser.GetUserName());
+                                ps.executeUpdate();
+                                DatabaseConnection.close();
+                            } catch (ClassNotFoundException | SQLException e) {
                                 SaveStackTrace.saveStackTrace(e);
                             }
+                        };
+                        Thread UpdateThread = new Thread(SQLUpdateThread);
+                        UpdateThread.start();
+                        UpdateThread.setName("SQL Update Thread");
+                        try {
+                            UpdateThread.join();
+                        } catch (InterruptedException e) {
+                            Logger logger = Server.GetInstance().logger;
+                            logger.error("发生异常InterruptedException");
+                            SaveStackTrace.saveStackTrace(e);
                         }
                     }
                 }
-                if (i == tmpClientIDAll) {
-                    break;
-                }
-                i = i + 1;
             }
         }
     }
